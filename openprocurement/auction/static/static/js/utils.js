6bea4a915411741d28e1bce08c6902ca23a8cd30
zalesky <zalesky@ukr.net>
2016-11-02 17:35:35 +0200

    Add tests.

--- openprocurement/auction/static/static/js/utils.js
+++ openprocurement/auction/static/static/js/utils.js
@@ -337,7 +337,6 @@ angular.module('auction')
       'get_round_data': get_round_data,
       'scroll_to_stage': scroll_to_stage,
       'parseQueryString': parseQueryString,
-      
       'stringifyQueryString': stringifyQueryString,
       'prepare_title_ending_data': prepare_title_ending_data,
       'pad': pad,
